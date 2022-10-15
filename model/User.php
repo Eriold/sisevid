46849e57c8c7469aa1b511262aeb32c4223e9a28
David Fernando Briceño Galindo <61285906+Dafbrig@users.noreply.github.com>
2022-10-15 01:09:45 -0500

    Model User.PHP
    Se crea el model de usuario para la parte de la validacion

--- model/User.php
+++ model/User.php
@@ -1,10 +1,10 @@
 <?
 class User{
-    private string $userIdUser;
+    private int $userIdUser;
     private string $userUsuario;
     private string $userPassword;
     private string $userCorreo;
-    private string $userIdRol;
+    private int $userIdRol;
 
     function __construct(string $userIdUser,
     string $userUsuario,
@@ -16,5 +16,80 @@ class User{
         $this->userPassword = $userPassword;
         $this->userCorreo = $userCorreo;
         $this->userIdRol = $userIdRol;
+    }
+
+    public function getUserIdUser()
+    {
+        return $this->userIdUser;
+    }
+    /**
+     * Set the value of userIdUser
+     *
+     * @return  self
+     */ 
+    public function setUserIdUser($userIdUser)
+    {
+        $this->userIdUser = $userIdUser;
+        return $this;
+    }
+    /**
+     * Get the value of userUsuario
+     */
+    public function getUserUsuario()
+    {
+        return $this->userUsuario;
+    }
+    /**
+     * Set the value of userUsuario
+     *@return self */
+    public function setUserUsuario($userUsuario)
+    {
+        $this->userUsuario = $userUsuario;
+        return $this;
+    }
+    /**
+     * Get the value of userPassword
+     */
+    public function getUserPassword()
+    {
+        return $this->userPassword;
+    }
+    /**
+     * Set the value of userPassword
+     *@return self */
+    public function setUserPassword($userPassword)
+    {
+        $this->userPassword = $userPassword;
+        return $this;
+    }
+    /**
+     * Get the value of userCorreo
+     */
+    public function getUserCorreo()
+    {
+        return $this->userCorreo;
+    }
+    /**
+     * Set the value of userCorreo
+     *@return self */
+    public function setUserCorreo($userCorreo)
+    {
+        $this->userCorreo = $userCorreo;
+        return $this;
+    }
+    /**
+     * Get the value of userIdRol
+     */
+    public function getUserIdRol()
+    {
+        return $this->userIdRol;
+    }
+    /**
+     * Set the value of userIdRol
+     *@return self */
+    public function setUserIdRol($userIdRol)
+    {
+        $this->userIdRol = $userIdRol;
+        return $this;
     }
 }
