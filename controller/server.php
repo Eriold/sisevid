3699a64bc0d377c0cf330520159efbf839da79f9
Daniel <dacamoty@gmail.com>
2022-10-27 22:50:25 -0500

    database local added

--- controller/server.php
+++ controller/server.php
@@ -1,5 +1,5 @@
 <?php 
-define('LOCALHOST', '162.241.2.168');
-define('USER', 'quickeon_alfa_test_log');
-define('PASSWORD', '3V{^ML5Odb&X');
-define('DATABASE', 'quickeon_sisevid');
+define('LOCALHOST', 'localhost');
+define('USER', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'sisevid');
